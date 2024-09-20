f6087a2fa75deaa409b659eeb987eab20a4d1c68
ADMIN <sarvarergashev776@gmail.com>
2024-09-20 18:23:38 +0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    compileTemplate: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    resetAsyncDataToUndefined: true,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+  },
+});
